5c92ec9caf5b64874c7718d54cfa2d1c0bca86c2
User <User@HP-LAPTOP>
2023-01-20 18:51:22 +0200

    Add project files.

---
+++ SummApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Summator
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            Summator.Test_SumTwoNumbers();
+        }
+
+    }
+}
+
+
+
